4bc4a3d2a32ef4dbe0b96f8aba4de35fd423bccd
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2025-05-16 11:06:14 +0200

    final code

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,27 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('should use first cb if condition is true', () => {
+    const firstCb = jest.fn(() => 'first');
+    const secondCb = jest.fn(() => 'second');
+    const condition = jest.fn(() => true);
 
+    ifElse(condition, firstCb, secondCb);
+
+    expect(firstCb).toHaveBeenCalled();
+    expect(secondCb).not.toHaveBeenCalled();
   });
 
-  // write tests here
+  it('should use the second cb if condition is false', () => {
+    const firstCb = jest.fn(() => 'first');
+    const secondCb = jest.fn(() => 'second');
+    const condition = jest.fn(() => false);
+
+    ifElse(condition, firstCb, secondCb);
+
+    expect(firstCb).not.toHaveBeenCalled();
+    expect(secondCb).toHaveBeenCalled();
+  });
 });
